f2505cd13206b4a94bf2964278b2217fa88525d9
ThibaultAjas <Thibault@mail.com>
2020-05-01 16:01:14 +0200

    [ADD] Ajout des events dans la vue "feed"

--- client/src/app/content/side_bars/feed/Feed.js
+++ client/src/app/content/side_bars/feed/Feed.js
@@ -1,18 +1,46 @@
 import React from "react";
 import FeedLine from "./FeedLine";
+import axios from 'axios';
 
 import '../../../stylesheets/feed.css';
 
-const Feed = () => {
-	return (
-		<div className='p-5 mx-lg-5 mx-md-2 mx-sm-2 feed'>
-			{
-				[...Array(10)].map( (element, index) => <FeedLine
-					image='https://www.opengovguide.com/wp-content/uploads/2019/07/rw-a-tool-for-sustainable-land-use-management-in-rwanda-homepage-780x439.jpg'
-					title='Title' descr='description'/> )
-			}
-		</div>
-	);
+class Feed extends React.Component {
+
+	state = {
+		events: []
+	};
+
+	componentDidMount = () => {
+		this.getEvents()
+	};
+
+	getEvents = () => {
+		axios.get('/api/getEvents')
+			.then((response) => {
+				const data = response.data;
+				this.setState({ events: data });
+				console.log('events have been received');
+			})
+			.catch((error) => {
+				console.log(`Error: ${error}`);
+			});
+	};
+
+	render() {
+		return (
+			<div className='p-5 mx-lg-5 mx-md-2 mx-sm-2 feed'>
+				{
+					[...Array(10)].map( (element, index) =>
+						<FeedLine
+						image='https://www.opengovguide.com/wp-content/uploads/2019/07/rw-a-tool-for-sustainable-land-use-management-in-rwanda-homepage-780x439.jpg'
+						title={element.title}
+						date={element.date}
+						descr={element.description}
+						/> )
+				}
+			</div>
+		);
+	}
 };
 
 export default Feed;

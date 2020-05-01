25fba65d6fa15ee1fa47e46a3ba9f58aa08d373b
ThibaultAjas <Thibault@mail.com>
2020-05-01 16:25:43 +0200

    [BUG] Correction 'undefined' dans feed.js

--- client/src/app/content/side_bars/feed/Feed.js
+++ client/src/app/content/side_bars/feed/Feed.js
@@ -30,7 +30,7 @@ class Feed extends React.Component {
         return (
             <div className='p-5 mx-lg-5 mx-md-2 mx-sm-2 feed'>
                 {
-                    [...Array(10)].map((element, index) =>
+                    this.state.events.map((element, index) =>
                         <FeedLine
                             image='https://www.opengovguide.com/wp-content/uploads/2019/07/rw-a-tool-for-sustainable-land-use-management-in-rwanda-homepage-780x439.jpg'
                             title={element.title}

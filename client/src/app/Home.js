e8686100318e950118d59eab5e517ac56df485d7
RibeyrollesMatthieu <ribeyrolles.matthieu@gmail.com>
2020-05-04 12:18:35 +0200

    [FIX] trying to fix side bars bugs

--- client/src/app/Home.js
+++ client/src/app/Home.js
@@ -3,10 +3,9 @@ import React from "react";
 import Header from "./content/header/Header";
 import ProfileBar from "./content/side_bars/profile_bar/ProfileBar";
 import FriendsBar from "./content/side_bars/friends_bar/FriendsBar";
-
-import './scripts/home_script';
-import './scripts/sideBars_script';
 import SwapFeedButtons from "./content/swap_feed_buttons/SwapFeedButtons";
+
+import './scripts/sideBars_script';
 
 const Home = ({ content, logged }) => {
 		return (
--- client/src/app/scripts/sideBars_script.js
+++ client/src/app/scripts/sideBars_script.js
@@ -1,80 +1,72 @@
-let PROFILE_BAR;
-let H_SHOW_PROFILE_BAR;
-let H_HIDE_PROFILE_BAR;
-let H_PROFILE_BAR_ACTIVATOR;
-let FRIENDS_BAR;
-let H_SHOW_FRIENDS_BAR;
-let H_HIDE_FRIENDS_BAR;
-let H_FRIENDS_BAR_ACTIVATOR;
+import $ from "jquery";
 
-let isProfileBarOpened = false;
-let isFriendsBarOpened = false;
+$(document).ready( () => {
+	const PROFILE_BAR = $('#profileBar');
+	const H_PROFILE_BAR_ACTIVATOR = $('.pb-activator');
+	const H_SHOW_PROFILE_BAR = $('#header-show-profileBar-icon');
+	const H_HIDE_PROFILE_BAR = $('#header-hide-profileBar-icon');
+	const FRIENDS_BAR = $('#friendsBar');
+	const H_FRIENDS_BAR_ACTIVATOR = $('.fb-activator');
+	const H_HIDE_FRIENDS_BAR = $("#header-hide-friendsBar-icon");
+	const H_SHOW_FRIENDS_BAR = $("#header-show-friendsBar-icon");
 
-let moveSideBar = ( bar ) => {
-	let marginSide = ( bar === PROFILE_BAR ) ? "margin-left" : "margin-right";
-	let currentMargin = parseInt(bar.css(marginSide), 10);
+	let isProfileBarOpened = false;
+	let isFriendsBarOpened = false;
 
-	if ( bar === PROFILE_BAR ) {
-		if ( marginSide === "margin-left" ) bar.animate({"margin-left" : (currentMargin === 0) ? '-300px' : '0'});
-	} else if ( bar === FRIENDS_BAR ) {
-		if ( marginSide === "margin-right" ) bar.animate({"margin-right" : (currentMargin === 0) ? '-300px' : '0'});
-	}
-};
+	let moveSideBar = ( bar ) => {
+		let marginSide = ( bar === PROFILE_BAR ) ? "margin-left" : "margin-right";
+		let currentMargin = parseInt(bar.css(marginSide), 10);
 
+		if ( bar === PROFILE_BAR ) {
+			if ( marginSide === "margin-left" ) bar.animate({"margin-left" : (currentMargin === 0) ? '-300px' : '0'});
+		} else if ( bar === FRIENDS_BAR ) {
+			if ( marginSide === "margin-right" ) bar.animate({"margin-right" : (currentMargin === 0) ? '-300px' : '0'});
+		}
+	};
 
-let hide_hideIcon = ( iconToHide, iconToShow ) => {
-	toggleElementVisibility( iconToShow, false );
-	setTimeout( () => {
-		toggleElementVisibility( iconToHide, true );
-	}, 400);
-};
+	let hide_hideIcon = ( iconToHide, iconToShow ) => {
+		toggleElementVisibility( iconToHide, false );
+		setTimeout( () => {
+			toggleElementVisibility( iconToShow, true );
+		}, 400);
+	};
 
-let activateProfileBar = () => {
-	if ( ! isFriendsBarOpened ) {
-		isProfileBarOpened = ! isProfileBarOpened;
-		moveSideBar( PROFILE_BAR );
+	let activateProfileBar = () => {
+		if ( ! isFriendsBarOpened ) {
+			isProfileBarOpened = ! isProfileBarOpened;
+			moveSideBar( PROFILE_BAR );
 
-		if (H_SHOW_PROFILE_BAR.is(':visible')) hide_hideIcon( H_HIDE_PROFILE_BAR, H_SHOW_PROFILE_BAR );
-		else hide_hideIcon( H_SHOW_PROFILE_BAR, H_HIDE_PROFILE_BAR );
-	}
-};
+			if (H_SHOW_PROFILE_BAR.is(':visible')) hide_hideIcon( H_SHOW_PROFILE_BAR, H_HIDE_PROFILE_BAR );
+			else hide_hideIcon( H_HIDE_PROFILE_BAR, H_SHOW_PROFILE_BAR  );
+		}
 
-let activateFriendsBar = () => {
-	if (! isProfileBarOpened) {
-		isFriendsBarOpened = ! isFriendsBarOpened;
-		moveSideBar( FRIENDS_BAR );
+		console.log(H_HIDE_PROFILE_BAR);
+	};
 
-		if (H_SHOW_FRIENDS_BAR.is(':visible')) hide_hideIcon( H_HIDE_FRIENDS_BAR, H_SHOW_FRIENDS_BAR );
-		else hide_hideIcon( H_SHOW_FRIENDS_BAR, H_HIDE_FRIENDS_BAR );
-	}
-};
+	let activateFriendsBar = () => {
+		if (! isProfileBarOpened) {
+			isFriendsBarOpened = ! isFriendsBarOpened;
+			moveSideBar( FRIENDS_BAR );
 
-let activateSideBar = ( bar ) => {
-	if ( bar === PROFILE_BAR ) {
-		activateProfileBar();
-	} else if ( bar === FRIENDS_BAR ) {
-		activateFriendsBar();
-	}
-};
+			if (H_SHOW_FRIENDS_BAR.is(':visible')) hide_hideIcon( H_SHOW_FRIENDS_BAR, H_HIDE_FRIENDS_BAR );
+			else hide_hideIcon( H_HIDE_FRIENDS_BAR, H_SHOW_FRIENDS_BAR );
+		}
+	};
+
+	let activateSideBar = ( bar ) => {
+		if ( bar === PROFILE_BAR ) {
+			activateProfileBar();
+		} else if ( bar === FRIENDS_BAR ) {
+			activateFriendsBar();
+		}
+	};
 
 //	fade(Out/In) a list of elements
-let toggleElementVisibility = ( element, toShow ) => {
-	if (toShow) element.fadeIn(0);
-	else 		element.fadeOut();
-};
+	let toggleElementVisibility = ( element, toShow ) => {
+		if (toShow) element.fadeIn(0);
+		else 		element.fadeOut();
+	};
 
-let initSideBarsComponents = (
-	profileBar, profileBarActivators, profileBarShowIcon, profileBarHideIcon,
-	friendsBar, friendsBarActivators, friendsBarShowIcon, friendsBarHideIcon ) => {
-
-	PROFILE_BAR = profileBar;
-	H_PROFILE_BAR_ACTIVATOR = profileBarActivators;
-	H_HIDE_PROFILE_BAR = profileBarHideIcon;
-	H_SHOW_PROFILE_BAR = profileBarShowIcon;
-	FRIENDS_BAR = friendsBar;
-	H_FRIENDS_BAR_ACTIVATOR = friendsBarActivators;
-	H_HIDE_FRIENDS_BAR = friendsBarHideIcon;
-	H_SHOW_FRIENDS_BAR = friendsBarShowIcon;
-};
-
-export  {initSideBarsComponents, activateSideBar} ;
+	H_PROFILE_BAR_ACTIVATOR.click( () => activateSideBar(PROFILE_BAR) );
+	H_FRIENDS_BAR_ACTIVATOR.click( () => activateSideBar(FRIENDS_BAR) );
+});

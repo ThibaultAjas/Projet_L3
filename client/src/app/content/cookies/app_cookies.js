8cc1ae824ae79b7351e7af9771a8652c6c760dc0
RibeyrollesMatthieu <ribeyrolles.matthieu@gmail.com>
2020-05-05 13:14:03 +0200

    [COMMENT] app_cookies.js

--- client/src/app/content/cookies/app_cookies.js
+++ client/src/app/content/cookies/app_cookies.js
@@ -1,19 +1,28 @@
 import Cookies from 'universal-cookie';
 
+/* define const to avoid typing errors */
 const 	IS_LOGGED 	= 'isLogged',
 		MAIL		= 'mail',
 		PASSWORD	= 'password';
 
+/* create and instantiate cookies */
 const appCookies = new Cookies('appCookies');
 
+
+
+/* Check and set if user is logged  */
 const isLogged = () => { return appCookies.get(IS_LOGGED) === 'true' };
 const setLogged = ( isLogged ) => { appCookies.set(IS_LOGGED, isLogged )};
 
+/* check and set session mail */
 const setSessionMail = ( mail ) => { appCookies.set(MAIL, mail) };
 const getSessionMail = () => { return appCookies.get(MAIL) };
 
+/* check and set session password */
 const setSessionPassword = ( password ) => { appCookies.set(PASSWORD, password) };
 const getSessionPassword = () => { return appCookies.get(PASSWORD) };
+
+
 
 export
 {

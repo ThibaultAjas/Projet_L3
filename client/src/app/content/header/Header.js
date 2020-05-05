2ceedb989452f7920c080d3058dc86e53c9e6379
RibeyrollesMatthieu <ribeyrolles.matthieu@gmail.com>
2020-05-05 13:25:41 +0200

    [CLEAR] header relative files

--- client/src/app/content/header/Header.js
+++ client/src/app/content/header/Header.js
@@ -1,31 +1,31 @@
 import React from "react";
-import '../../stylesheets/header.css';
 
 import { isLogged } from "../cookies/app_cookies";
+
+import '../../stylesheets/header.css';
+
 
 const Header = () => {
 	return (
 		<header className={'bg-dark mx-auto w-100 px-2 d-flex ' + ((isLogged()) ? 'justify-content-between' : 'justify-content-center')}>
 			{
 				(isLogged())	?
-								<div id='profileBar-activators' className='pt-2 pb-activator'>
-									<div id="header-show-profileBar-icon" className='rounded-circle element'/>
-									<i id="header-hide-profileBar-icon" className='rounded-circle element fas fa-angle-double-left'
-									   style={ { 'display': 'none' } } />
-								</div>
-							: <> </>
+									<div id='profileBar-activators' className='pt-2 pb-activator'>
+										<div id="header-show-profileBar-icon" className='rounded-circle element'/>
+										<i id="header-hide-profileBar-icon" className='rounded-circle element fas fa-angle-double-left' style={ { 'display': 'none' } } />
+									</div>
+								: <> </>
 			}
 
 			<a id='h-title' href='/'><h1> GREENSTAGRAM </h1></a>
 
 			{
 				(isLogged())	?
-								<div id='friendsBar-activators' className='pt-2 fb-activator'>
-									<i id="header-show-friendsBar-icon" className='rounded-circle element fa fa-users'/>
-									<i id="header-hide-friendsBar-icon" className='rounded-circle element fas fa-angle-double-right'
-									   style={ { 'display': 'none' } }/>
-								</div>
-							: <> </>
+									<div id='friendsBar-activators' className='pt-2 fb-activator'>
+										<i id="header-show-friendsBar-icon" className='rounded-circle element fa fa-users'/>
+										<i id="header-hide-friendsBar-icon" className='rounded-circle element fas fa-angle-double-right' style={ { 'display': 'none' } }/>
+									</div>
+								: <> </>
 			}
 		</header>
 	)

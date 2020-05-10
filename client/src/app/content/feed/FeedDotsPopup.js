1a200cfe27fa18e2fe2eda6f61d2d14460b2550f
RibeyrollesMatthieu <ribeyrolles.matthieu@gmail.com>
2020-05-10 12:49:35 +0200

    [ADD] we can now go to owner profile of a post when on feed [ADD] event display

--- client/src/app/content/feed/FeedDotsPopup.js
+++ client/src/app/content/feed/FeedDotsPopup.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-const options =
-[
-	"Report",
-	"Hide from feed",
-	"See profile"
-];
+const FeedDotsPopup = ({ userId }) => {
 
-const FeedDotsPopup = () => {
+	const options =
+		[
+			{ text: "Report", href: '/report/' + userId },
+			// { text: "Hide from feed", href: '/' },
+			{ text: "See profile", href: '/profile/' + userId }
+		];
+
 	return (
 		<div className='dotsPopup card align-items-end d-inline-block'>
 			{
-				options.map( option => <a href='/' className='dots-option text-right p-1'> { option } </a> )
+				options.map( option => <a href={option.href} className='dots-option text-right p-1'> { option.text } </a> )
 			}
 		</div>
 	);

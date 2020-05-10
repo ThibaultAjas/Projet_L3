aa2dc9e84c2851f81ae678efb6495abb194c0501
RibeyrollesMatthieu <ribeyrolles.matthieu@gmail.com>
2020-05-10 15:36:58 +0200

    [FIX] fixing console logs

--- client/src/app/content/event/EventDisplay.js
+++ client/src/app/content/event/EventDisplay.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { getEventFromId, getFriendsById, getUserEventList } from "../util/dataConverter";
-import { getUser } from "../util/app_cookies";
+import { getEventFromId } from "../util/dataConverter";
 import FeedLine from "../feed/FeedLine";
 
 class EventDisplay extends React.Component {
@@ -9,7 +8,6 @@ class EventDisplay extends React.Component {
 
 	componentDidMount() {
 
-		// console.log(new URL(window.location.href).pathname.substring(7));
 		getEventFromId(new URL(window.location.href).pathname.substring(7)).then((data) => {
 			this.event = data.data; /* ou juste data ? */
 		})

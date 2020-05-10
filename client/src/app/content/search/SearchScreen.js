9ad54bc7223b3066f05f50a9c85e986bc50d0632
RibeyrollesMatthieu <ribeyrolles.matthieu@gmail.com>
2020-05-10 23:51:36 +0200

    [FIX] search live display

--- client/src/app/content/search/SearchScreen.js
+++ client/src/app/content/search/SearchScreen.js
@@ -1,49 +1,58 @@
 import React from "react";
 import SearchResults from "./SearchResults";
-import { getUser } from "../util/app_cookies";
 import { search_user } from "../util/search";
 
 class SearchScreen extends React.Component {
 
-
 	constructor(props) {
-		super(props);
+		super( props );
 
 		this.state = {
 			content: [],
 			lookingForUser: true
 		};
 
+		this.search = '';
+
 		this.handleChange = this.handleChange.bind(this);
 	}
 
+
+
 	componentDidMount() {
-		this.setState({ content: search_user('') });
+		search_user(this.search).then((data) => {
+			this.setState({ content: data});
+			this.forceUpdate();
+		});
 	}
+
 
 	handleChange({ target }) {
-		const { name, value } = target;
-		this.setState({ content:  search_user(value)});
+		const { name, value} = target;
+
+		search_user(value).then((data) => {
+			this.setState({ content: data });
+		});
+
+		this.search = value;
+
+		console.log('xd: ', this.search);
 	}
 
-	searchUser() {
-		this.setState({ content: search_user('aze')});
-		this.setState({ lookingForUser: true })}
-	searchEvent() { this.setState({ lookingForUser: false })}
+	searchPerson() { this.setState({ lookingForUser: true}); }
+	searchEvent() { this.setState({ lookingForUser: false}); }
+
 
 	render() {
 		return (
 			<>
 				<div className='w-100 bg-secondary d-inline-flex justify-content-center p-3 align-items-center'>
-					<input type='text' placeholder='person/event name' name='content' className='p-1 mr-2' onChange={this.handleChange} />
-
-					<input type='button' className='p-1 btn btn-warning mr-2' value='Search for person' onClick={() => this.searchUser()}/>
+					<input type='text' placeholder='person/event name' className='p-1 mr-2' name='x' onChange={this.handleChange}/>
+					<input type='button' className='p-1 btn btn-warning mr-2' onClick={() => this.searchPerson()} value='Search for person'/>
 					<input type='button' className='p-1 btn btn-warning' onClick={() => this.searchEvent()} value='Search for event'/>
 				</div>
 
-				{/*<SearchResults content={getUser().following} isUser={this.state.lookingForUser}/>*/}
-
-				{ this.state.content.map((state) => console.log(state)) }
+				<SearchResults content={(this.state.content === undefined) ? [] : this.state.content} isUser={this.state.lookingForUser} search={this.search}/>
 			</>
 		);
 	}

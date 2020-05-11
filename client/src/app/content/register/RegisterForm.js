2f8f30cfb92757c47031ffd2ccbff77bc2b8d461
bruno <brunopierre.mzl@gmail.com>
2020-05-11 14:06:41 +0200

    [FIX] verif all param and check if right or wrong on RegisterForm.js

--- client/src/app/content/register/RegisterForm.js
+++ client/src/app/content/register/RegisterForm.js
@@ -22,10 +22,30 @@ class RegisterForm extends React.Component {
 
     };
 
+
+    formConform = () => {
+        let tmp = true;
+
+        if (this.state.password !== this.state.password_conf){
+            return(false);
+        };
+        if(this.state.firstName.length>15 | this.state.lastName.length>15 | this.state.userName.length>20){
+            return(false);
+        };
+        if(this.state.tel.length!==10){
+            return(false);
+        };
+        if(this.state.address.length>50){
+            return(false);
+        }else {
+            return (tmp);
+        };
+    };
+
     submit = (event) => {
         event.preventDefault();
 
-        if (this.state.password === this.state.password_conf) {
+        if (this.formConform()) {
             const payload = {
                 mail: this.state.mail,
                 password: this.state.password,
@@ -56,10 +76,47 @@ class RegisterForm extends React.Component {
                     // TODO: Afficher une popup, une div ... Pour signaler une erreur à l'utilisateur
                 });
         }else{
-            document.getElementById("password").value = "";
-            document.getElementById("confPassword").value = "";
-            document.getElementById("password").style.backgroundColor = "#db8491";
-            document.getElementById("confPassword").style.backgroundColor = "#db8491";
+            if (this.state.password !== this.state.password_conf) {
+                document.getElementById("password").value = "";
+                document.getElementById("confPassword").value = "";
+                document.getElementById("password").style.backgroundColor = "#db8491";
+                document.getElementById("confPassword").style.backgroundColor = "#db8491";
+            }else{
+                document.getElementById("password").style.backgroundColor = "white";
+                document.getElementById("confPassword").style.backgroundColor = "white";
+            };
+            if (this.state.firstName.length>15){
+              document.getElementById("firstName").value = "";
+              document.getElementById("firstName").style.background = "#db8491";
+            }else{
+                document.getElementById("firstName").style.background = "white";
+            }
+            if( this.state.lastName.length>15 ){
+                document.getElementById("lastName").value = "";
+                document.getElementById("lastName").style.background = "#db8491";
+            }else{
+                document.getElementById("lastName").style.background = "white";
+            }
+            if(this.state.userName.length>20){
+                document.getElementById("userName").value = "";
+                document.getElementById("userName").style.background = "#db8491";
+            }else{
+                document.getElementById("userName").style.background = "white";
+            }
+            if(this.state.tel.length!==10){
+                document.getElementById("tel").value = "";
+                document.getElementById("tel").style.background = "#db8491";
+            }else{
+                document.getElementById("tel").style.background = "white";
+            }
+            if(this.state.address.length>50){
+                document.getElementById("address").value = "";
+                document.getElementById("address").style.background = "#db8491";
+            }else{
+                document.getElementById("address").style.background = "white";
+            }
+
+
         };
     };
     resetUserInputs = () => {
@@ -80,17 +137,17 @@ class RegisterForm extends React.Component {
 
                 <h1 className="h3 mb-3 font-weight-normal "> Please sign in</h1>
 
-                <input type="email" className="form-control inputreg" placeholder="Email address" name="mail" required onChange = { this.handleChange } autoFocus/>
-                <input type="password" className="form-control inputreg" placeholder="Password" name="password" id="password" required onChange = { this.handleChange } autoFocus/>
-                <input type="password" className="form-control inputreg" placeholder="confirm password" name="password_conf" id="confPassword" onChange = { this.handleChange }  required/>
-                <input type="text" className="form-control inputreg" placeholder="first name" name="firstName" onChange = { this.handleChange }  required/>
-                <input type="text" className="form-control inputreg" placeholder="last name" name="lastName" onChange = { this.handleChange } required/>
-                <input type="text" className="form-control inputreg" placeholder="username" name="userName" onChange = { this.handleChange } />
-                <input type="tel" className="form-control inputreg" placeholder="0675488456" name="tel" onChange = { this.handleChange }  required/>
-                <input type="text" className="form-control inputreg" placeholder="addresse" name="address" onChange = { this.handleChange }  required/>
-                <input type="text" className="form-control inputreg" placeholder="code postal" name="code_postal" onChange = { this.handleChange }  required/>
-                <input type="text" className="form-control inputreg" placeholder="ville" name="city" onChange = { this.handleChange }  required/>
-                <input type="text" className="form-control inputreg" placeholder="country" name="country" onChange = { this.handleChange }  required/>
+                <input type="email" className="form-control inputreg"    placeholder="Email address"    name="mail"          id="mail"              onChange = { this.handleChange }  required autoFocus/>
+                <input type="password" className="form-control inputreg" placeholder="Password"         name="password"      id="password"          onChange = { this.handleChange }  required/>
+                <input type="password" className="form-control inputreg" placeholder="confirm password" name="password_conf" id="confPassword"     onChange = { this.handleChange }  required/>
+                <input type="text" className="form-control inputreg"     placeholder="first name"       name="firstName"     id="firstName"         onChange = { this.handleChange }  required/>
+                <input type="text" className="form-control inputreg"     placeholder="last name"        name="lastName"      id="lastName"          onChange = { this.handleChange }  required/>
+                <input type="text" className="form-control inputreg"     placeholder="username"         name="userName"      id="userName"          onChange = { this.handleChange }  required/>
+                <input type="tel" className="form-control inputreg"      placeholder="0675488456"       name="tel"           id="tel"               onChange = { this.handleChange }  required/>
+                <input type="text" className="form-control inputreg"     placeholder="addresse"         name="address"       id="address"           onChange = { this.handleChange }  required/>
+                <input type="text" className="form-control inputreg"     placeholder="code postal"      name="code_postal"   id="code_postal"       onChange = { this.handleChange }  required/>
+                <input type="text" className="form-control inputreg"     placeholder="ville"            name="city"          id="city"              onChange = { this.handleChange }  required/>
+                <input type="text" className="form-control inputreg"     placeholder="country"          name="country"       id="country"           onChange = { this.handleChange }  required/>
 
                 <div className="checkbox mb-3">
                     <input type="checkbox" value="remember-me"/>

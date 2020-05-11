9c22226e051c951420385d1007014c08e9a8964b
ThibaultAjas <Thibault@mail.com>
2020-05-11 11:49:49 +0200

    [FIX] event/findOne

--- routes/event.js
+++ routes/event.js
@@ -54,7 +54,7 @@ const event = require('../models/eventModel');
     router.post('/getOne', (req, res) => {
         const data = event.body;
 
-        event.find(data)
+        event.findOne(data)
             .then((data) => {
                 return res.json({msg: 'Got event', data: data});
             })
@@ -66,21 +66,29 @@ const event = require('../models/eventModel');
     router.post('/is_dis_likedByUser', (req, res) => {
         const data = req.body;
 
-        event.findOne(data.event)
-            .then(evt => {
-                switch (data.action) {
-                    case 'like':
-                        return res.json({msg:'', data: evt.usersWhoLiked.find(e => e.toString() === data.user._id.toString()) !== undefined});
-                    case 'dislike':
-                        return res.json({msg:'', data: evt.usersWhoDisliked.find(e => e.toString() === data.user._id.toString()) !== undefined});
-                    default:
-                        return res.json({msg:'', data: false});
-                }
-            })
-            .catch((error) => {
-                console.log(error)
-                return res.status(500).send(error);
-            });
+        if (data.action === 'like' || data.action === 'dislike') {
+            event.findOne(data.event)
+                .then(evt => {
+                    switch (data.action) {
+                        case 'like':
+                            return res.json({
+                                msg: '',
+                                data: evt.usersWhoLiked.find(e => e.toString() === data.user._id.toString()) !== undefined
+                            });
+                        case 'dislike':
+                            return res.json({
+                                msg: '',
+                                data: evt.usersWhoDisliked.find(e => e.toString() === data.user._id.toString()) !== undefined
+                            });
+                        default:
+                            return res.json({msg: '', data: false});
+                    }
+                })
+                .catch((error) => {
+                    console.log(error)
+                    return res.status(500).send(error);
+                });
+        } else return res.json({msg: '', data: false});
     })
 
     router.post('/getAllForUser', (req, res) => {
